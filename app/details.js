dc3e1ce5f44b7883e388db4bd4c78e487029b0fb
S-Guettner-Dev <sven_guettner@protonmail.com>
2023-04-11 20:56:24 +0200

    feat: added expo Routing

--- app/details.js
+++ app/details.js
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, View , SafeAreaView} from "react-native";
+import { Text, View , SafeAreaView} from "react-native";
 import {Link} from "expo-router"
 
 const details = () => {
@@ -8,7 +8,7 @@ const details = () => {
             <Link href='/'>Home</Link>
         </SafeAreaView >
 
-     );
+     )
 }
  
-export default details;
+export default details
--- app/index.js
+++ app/index.js
@@ -1,15 +1,40 @@
-import { StyleSheet, Text, View , SafeAreaView} from "react-native";
+import { Text, View , SafeAreaView} from "react-native"
 import {Link} from "expo-router"
+import { useEffect , useState} from "react"
+
 
 export default function Page() {
+  
+  const [serverPing,setServerPing] = useState()
+  const [coinData,setCoinData] = useState([])
+  
+  useEffect(() => {
+    fetch('https://api.coingecko.com/api/v3/ping')
+    .then(res => res.json())
+    .then(data => {
+      console.log(data.gecko_says)
+      data.gecko_says ? setServerPing(true) : setServerPing(false)
+    })
+    /* .then(fetch('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false&locale=en')
+          .then(res => res.json())
+          .then(data => {
+            console.log(data)
+            setCoinData(data)
+          })
+    ) */
+  },[])
+
+  
   return (
     
       <SafeAreaView className="pt-10">
-        <Text className="text-4xl text-red-700">Hello World ddddd</Text>
+        <Text className="text-4xl text-red-700">Hello World</Text>
         <Text >This is the first page of your app.</Text>
-        <Link href='/details'>details</Link>
+        <Link className="border-2 p-2 " href='/details'>details</Link>
+        <Text>server online: {`${serverPing}`}</Text>
+        
       </SafeAreaView>
     
-  );
+  )
 }
 

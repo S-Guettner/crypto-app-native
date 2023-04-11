2d85d5b3fb2e8713a3b99d20ea3ef487b6259dc3
S-Guettner-Dev <sven_guettner@protonmail.com>
2023-04-11 17:56:38 +0200

    setup react native with expo && nativewind

---
+++ app/index.js
@@ -0,0 +1,13 @@
+import { StyleSheet, Text, View , SafeAreaView} from "react-native";
+
+export default function Page() {
+  return (
+    
+      <SafeAreaView >
+        <Text className="text-4xl text-red-700">Hello World</Text>
+        <Text >This is the first page of your app.</Text>
+      </SafeAreaView>
+    
+  );
+}
+
